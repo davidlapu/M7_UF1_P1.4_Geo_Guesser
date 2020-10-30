9136737d4e6a2612091a74e9d76bac30a173c57f
David Lama Pulido <david.lama.7e3@itb.cat>
2020-10-30 16:27:36 +0100

    Initial commit

---
+++ app/src/main/java/cat/itb/m7_uf1_p14_geo_guesser/MainActivity.java
@@ -0,0 +1,14 @@
+package cat.itb.m7_uf1_p14_geo_guesser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/cat/itb/m7_uf1_p14_geo_guesser/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package cat.itb.m7_uf1_p14_geo_guesser;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
